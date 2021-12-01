a7c14c1f748481bfb9bf2546889f03997343b698
Emma Brundell <emma.brundell@prevas.se>
2021-12-01 13:42:42 +0100

    day 1 done

--- ett/ett.js
+++ ett/ett.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./input.txt', {encoding:'utf8', flag:'r'});
-arrayOfValues = data.split('\n');
+arrayOfValues = data.split('\n').map(x => parseInt(x));
 
 let priorValue = 0;
-let increaseCount = 0;
+let increaseCount = -1;
 arrayOfValues.forEach((value) => {
     if(value > priorValue) {
       increaseCount++;
@@ -16,11 +16,11 @@ arrayOfValues.forEach((value) => {
 console.log("result: " + increaseCount);
 
 increaseGroupCount = 0;
+
 for(i = 0; i < arrayOfValues.length; i++) {
 
   if(arrayOfValues[i] < arrayOfValues[i + 3] ) {
     increaseGroupCount++;
   }
-  console.log(arrayOfValues[i + 3] + " " + arrayOfValues[i] + " count: " + increaseGroupCount );
 }
 console.log("group result: " + increaseGroupCount);
